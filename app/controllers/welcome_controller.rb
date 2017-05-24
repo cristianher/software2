849f6aa80f62b21f4c2817ab3b58a9d0997c6b82
Cristian Hernandez <cristianwhite3@gmail.com>
2017-05-24 16:19:11 -0500

    ultimos cambios

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,13 +8,26 @@ class WelcomeController < ApplicationController
     end
   end
 
-  def statistics
-  	arr = Array.new
-  	@p = Alert.where(level: 3).count
-  	@m = Alert.where(level: 2).count
-  	@l = Alert.where(level: 1).count
-  	@months = Alert.group_by_month(:created_at).count
-  	@alerts = Alert.group_by_day(:created_at).count
-  	@aalerts = Alert.all
-  end
+	def statistics
+
+		if(params[:month].present?)
+			st_date = Date.parse(params[:month])
+			date_range = st_date.beginning_of_month..st_date.end_of_month
+			arr = Array.new
+			@p = Alert.where(level: 3).where(:created_at =>  date_range).count
+			@m = Alert.where(level: 2).where(:created_at =>  date_range).count
+			@l = Alert.where(level: 1).where(:created_at =>  date_range).count
+			@months = Alert.where(:created_at =>  date_range).group_by_month(:created_at).count
+			@alerts = Alert.where(:created_at =>  date_range).group_by_day(:created_at).count
+			@aalerts = Alert.all.where(:created_at =>  date_range)
+		else
+			arr = Array.new
+			@p = Alert.where(level: 3).count
+			@m = Alert.where(level: 2).count
+			@l = Alert.where(level: 1).count
+			@months = Alert.group_by_month(:created_at).count
+			@alerts = Alert.group_by_day(:created_at).count
+			@aalerts = Alert.all
+		end
+	end
 end
